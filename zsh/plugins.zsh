90e5f6290416246c98b2be4563a7074a269d6a22
Dominik Bernroider <dominik.bernroider@icloud.com>
2024-02-16 03:44:56 +0100

    remove z in favor of zoxide

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,7 +2,6 @@ github_plugins=(
   zsh-users/zsh-autosuggestions
   zsh-users/zsh-completions
   zsh-users/zsh-history-substring-search
-  rupa/z
   zdharma/fast-syntax-highlighting
   romkatv/powerlevel10k
 )
