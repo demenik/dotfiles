a6531579880ee4e4d3facf051f99331024938c01
Dominik Bernroider <dominik.bernroider@icloud.com>
2024-02-16 03:46:11 +0100

    remove tmux (last one i swear)

--- setup.zsh
+++ setup.zsh
@@ -4,7 +4,6 @@ SCRIPT_DIR="${0:A:h}"
 
 echo "Linking config files..."
 ln -nfs $SCRIPT_DIR/zsh/.zshrc $HOME/.zshrc
-ln -nfs $SCRIPT_DIR/tmux/.tmux.conf $HOME/.tmux.conf
 ln -nfs $SCRIPT_DIR/config/.nvmrc $HOME/.nvmrc
 ln -nfs $SCRIPT_DIR/config/.gitconfig $HOME/.gitconfig
 echo "  ..done"
